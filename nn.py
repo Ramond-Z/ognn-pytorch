5d9daca54b634102ca9180bfe0c96b6e51059971
RaymondZhou <1138541250@qq.com>
2025-10-13 06:36:39 +0000

    implementation of Monte Carlo Graph conv, pytorch version and triton version with k = 1

--- nn.py
+++ nn.py
@@ -11,9 +11,9 @@ import torch.nn.functional as F
 
 
 from torch.utils.checkpoint import checkpoint
-from ..ocnn.nn import OctreeGroupNorm
-from .octreed import OctreeD
-from .utils import scatter_mean
+from ocnn.nn import OctreeGroupNorm
+from octreed import OctreeD
+from utils import scatter_mean
 
 
 class Activation(torch.nn.Module):
@@ -94,6 +94,134 @@ class GraphConv(torch.nn.Module):
     return ('in_channels={}, out_channels={}, n_edge_type={}, n_node_type={}, '
             'use_bias={}'.format(self.in_channels, self.out_channels,
              self.n_edge_type, self.n_node_type, self.use_bias))  # noqa
+
+from flex_gemm.kernels.triton.spconv import sparse_submanifold_conv_fwd_implicit_gemm_splitk, sparse_submanifold_conv_bwd_implicit_gemm_splitk
+
+
+def flex_gemm_forward_implicit(data: torch.Tensor, weight: torch.Tensor, bias: torch.Tensor, neighbour: torch.Tensor):
+    return sparse_submanifold_conv_fwd_implicit_gemm_splitk(data, weight, bias, neighbour, -1)
+
+
+def flex_gemm_backward_implicit(grad: torch.Tensor, weight: torch.Tensor, data: torch.Tensor, neighbour: torch.Tensor):
+    return sparse_submanifold_conv_bwd_implicit_gemm_splitk(grad, data, weight, None, neighbour, -1)
+
+
+class _flex_gemm(torch.autograd.Function):
+    @staticmethod
+    def forward(data, weight, neighbour):
+        result = flex_gemm_forward_implicit(data, weight, None, neighbour)
+        return result
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        data, weight, neighbour = inputs
+        ctx.save_for_backward(data, weight, neighbour)
+
+    @staticmethod
+    def backward(ctx, upstream_grad):
+        data, weight, neighbour = ctx.saved_tensors
+        if ctx.needs_input_grad[0] or ctx.needs_input_grad[1]:
+            upstream_grad = upstream_grad.contiguous()
+        g_data, g_weight, _ = flex_gemm_backward_implicit(upstream_grad, weight, data, neighbour)
+        return g_data, g_weight, None
+
+
+def flex_gemm(data, weight, neighbour):
+    return _flex_gemm.apply(data, weight, neighbour)
+
+
+def select_k_sorted(data: torch.Tensor, idx: torch.Tensor, k: int, max_idx: int = None):
+    unique_idx, counts = torch.unique_consecutive(idx, return_counts=True)
+    M = unique_idx.size(0)
+    max_idx =  M + 1 if max_idx is None else max_idx
+    D = data.size(-1)
+    result = torch.zeros((max_idx, k, D), dtype=data.dtype, device=data.device)
+    starts = torch.cat([torch.zeros((1,), device=idx.device, dtype=torch.long), counts.cumsum(0)[:-1]])
+    rel_idx = torch.randint(0, counts.max(), (M, k), device=idx.device) % counts.unsqueeze(1)
+    global_idx = starts.unsqueeze(1) + rel_idx
+    result[unique_idx, :, :] = data[global_idx]
+    return result
+
+
+def select_k_indices_sorted(idx: torch.Tensor, k: int, max_idx: int):
+   unique_idx, counts = torch.unique_consecutive(idx, return_counts=True)
+   results = torch.ones((max_idx, k), device=idx.device, dtype=torch.long) * -1
+   starts = torch.cat([torch.zeros((1, ), device=idx.device, dtype=torch.long), counts.cumsum(0)[:-1]])
+   rel_idx = torch.randint(0, counts.max(), (unique_idx.size(0), k), dtype=torch.long, device=idx.device) % counts.unsqueeze(1)
+   global_idx = starts.unsqueeze(1) + rel_idx
+   results[unique_idx, :] = global_idx // 7
+   return results
+
+
+def select_k(data: torch.Tensor, idx: torch.Tensor, k: int, max_idx: int = None):
+    sort_idx = torch.argsort(idx)
+    return select_k_sorted(data[sort_idx], idx[sort_idx], k, max_idx)
+
+
+def sort_by_key(data: torch.Tensor, key: torch.Tensor):
+    sort_idx = torch.argsort(key)
+    return data[sort_idx], key[sort_idx]
+
+
+class GraphConvMonteCarlo(torch.nn.Module):
+
+  def __init__(
+          self, in_channels: int, out_channels: int, n_edge_type: int = 7,
+          n_node_type: int = 0, use_bias: bool = False, n_samples: int = 2, use_triton: bool = False):
+    super().__init__()
+    self.avg_degree = 7
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.n_edge_type = n_edge_type
+    self.n_node_type = n_node_type
+    self.use_bias = use_bias
+    self.n_samples = n_samples
+    self.use_triton = use_triton
+
+    node_channel = n_node_type if n_node_type > 1 else 0
+    self.weights = torch.nn.Parameter(
+        torch.randn(n_edge_type * (in_channels + node_channel), out_channels))
+    if self.use_bias:
+      self.bias = torch.nn.Parameter(torch.randn(out_channels))
+
+  def forward(self, x: torch.Tensor, octree, depth: int):
+    graph = octree.graphs[depth]
+
+    # concatenate the one_hot vector for node_type
+    if self.n_node_type > 1:
+      one_hot = F.one_hot(graph.node_type, num_classes=self.n_node_type)
+      x = torch.cat([x, one_hot], dim=1)
+
+    # x -> col_data
+    if not self.use_triton:
+        row, col = graph.edge_idx
+        index = row * self.n_edge_type + graph.edge_type
+        col_data = select_k_sorted(
+            x[col], index, self.n_samples, max_idx=x.shape[0] * self.n_edge_type).mean(1)
+
+        # add self-loops
+        index = torch.arange(graph.nnum, dtype=torch.int64, device=x.device)
+        index = index * self.n_edge_type + (self.n_edge_type - 1)
+        col_data[index] = x
+
+        # matrix product
+        output = col_data.view(x.shape[0], -1) @ self.weights
+    else:
+        assert self.n_samples == 1
+        row, col = graph.edge_idx
+        index = row * self.n_edge_type + graph.edge_type
+        selected_neighbour = select_k_indices_sorted(index, self.n_samples, x.shape[0] * self.n_edge_type).view(-1)
+        # print(selected_neighbour.shape)
+        idx = torch.arange(graph.nnum, dtype=torch.long, device=x.device)
+        idx = idx * self.n_edge_type + (self.n_edge_type - 1)
+        selected_neighbour[idx] = torch.arange(graph.nnum, dtype=torch.long, device=x.device)
+        selected_neighbour = selected_neighbour.reshape(-1, self.n_edge_type).contiguous()
+        output = flex_gemm(x.contiguous(), self.weights.reshape(self.out_channels, self.n_edge_type, -1).contiguous(), selected_neighbour)
+
+    # add bias
+    if self.use_bias:
+      output += self.bias
+    return output
 
 
 class GraphNorm(torch.nn.Module):
